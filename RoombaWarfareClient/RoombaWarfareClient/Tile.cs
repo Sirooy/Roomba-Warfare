fe70437a865b5ae391c660fa8ecda60853250d42
Siroy <victor_tob8@hotmail.com>
2018-05-18 18:33:14 +0200

    Rename class Block to Tile

--- RoombaWarfareClient/RoombaWarfareClient/Tile.cs
+++ RoombaWarfareClient/RoombaWarfareClient/Tile.cs
@@ -1,7 +1,12 @@
 ﻿
-public class Block
+public class Tile : Entity
 {
     public static readonly byte SPRITE_WIDTH = 64;
     public static readonly byte SPRITE_HEIGHT = 64;
     //TO DO
+
+    public void Render()
+    {
+        //TO DO
+    }
 }
